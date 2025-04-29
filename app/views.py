a2983446f960d0969b09984b452bb0d2fed5c2d6
katwojppuz <katarzyna.wojcik@ppuz.edu.pl>
2025-04-29 11:13:20 +0200

    Product id form template

--- app/views.py
+++ app/views.py
@@ -1,12 +1,17 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for, request
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/extract')
-def extract():
+@app.route('/extract', methods=['post'])
+def extract_data():
+    product_id = request.form.get('product_id')
+    return redirect(url_for('product', product_id=product_id))
+
+@app.route('/extract', methods=['get'])
+def display_form():
     return render_template("extract.html")
 
 @app.route('/products')
